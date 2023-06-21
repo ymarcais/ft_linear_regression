5489f290bef974acc0d970d6ac038536be2bb355
Yves MARCAIS <ymarcais@paul-f3Ar4s10.clusters.42paris.fr>
2023-06-21 12:59:47 +0200

    ok but without norminette

--- gradient_descent.py
+++ gradient_descent.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from sklearn.preprocessing import StandardScaler #pip install scikit-learn
 
+# dataclass init instances
 @dataclass
 class GradientDescent:
 	thetas: np.ndarray
@@ -21,17 +22,15 @@ class GradientDescent:
 			print("ERROR: x Numpy Array")
 			exit()
 
-	#y_hat = X * theta
+	#y prediction: y_hat = X * theta
 	def predict_(self, x):
 		X = self.add_one_column(x)
 		y_hat = np.matmul(X, self.thetas)
 		return y_hat
-
 	#tmp_theta(0) = (1/m)sum((hθ(x(i) ) − y(i)))
 	#tmp_theta(0) = (1/m)sum((hθ(x(i) ) − y(i))) * 1 
 	#tmp_theta(0) = (1/m)sum((hθ(x(i) ) − y(i))) * x0(i) // rewrite 1 as x0(i) :
 	#tmp_theta(1) = (1/m)sum((hθ(x(i) ) − y(i))) * x1(i)
-	#tmp_theta(j) = (1/m)sum((hθ(x(i) ) − y(i))) * xj(i)
 	#Vectorisation:
 		# hθ (x) = X' * θ    // hθ (x) = θ0 + θ1 x
 		# tmp_theta(j) = (1 / m) * (X' * θ - y) * X'(j)
@@ -41,11 +40,12 @@ class GradientDescent:
 		X = self.add_one_column(x)
 		y_hat = self.predict_(x)
 		cost = (y_hat - y)
-		print("cost", np.sum(cost))
+		#print("cost", np.sum(cost))
 		gradient = (1/(2 * m)) * np.dot(X.T, cost)
 		return gradient
 
-	def fit_(self, x, y, thetas, alpha, max_iter, epsilon=1e-5):
+	# gradient descent
+	def fit_(self, x, y, thetas, alpha, max_iter, epsilon=1e-3):
 		i= 0
 		m = len(x)
 		prev_cost = 10.0
@@ -59,11 +59,13 @@ class GradientDescent:
 			#print("thetas: ", thetas)
 			y_hat = self.predict_(x)
 			current_cost = (1 / (2 * m)) * np.sum((y_hat - y)**2)
-			if current_cost - prev_cost < epsilon :
+			#print("current cost: ", current_cost)
+			#print("prev_cost: ", prev_cost)
+			if abs(current_cost - prev_cost) < epsilon :
 				print("Converged at iteration", i+1)
 				break
 			alpha *= 0.99999
-			#print("current cost: ", current_cost)
+			prev_cost = current_cost
 		return thetas
 
 	def plot(self, x_scaled, x, y, thetas, new_y_hat):
@@ -81,7 +83,7 @@ class GradientDescent:
 		axs[1].set_title("Prediction")
 		axs[1].set_xlabel("km")
 		axs[1].set_ylabel("Price")
-		axs[1].xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.1f}'))
+		axs[1].xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
 		axs[1].yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
 
 		plt.tight_layout()
@@ -106,21 +108,13 @@ def main():
 	x_scaled = scaler.fit_transform(x)
 	max_iter = gd.max_iter
 	thetas = gd.fit_(x_scaled, y, thetas, alpha, max_iter)
-	print("thetas:", thetas)
-	
-	'''thetas[0] = 10000
-	thetas[1] = -0.025
-	y_hat = gd.predict_(x)
-	#theo_cost = (1 / (2 * m)) * np.sum((y_hat - y)**2)
-	theo_cost = np.sum((y_hat - y))
-	print("theo_cost =", theo_cost)'''
+	#print("thetas:", thetas)
 	new_predict = np.array([50000]).reshape((-1, 1))
 	new_predict = scaler.transform(new_predict)
 	new_y_hat = gd.predict_(new_predict)
-	print("new_predict", new_predict)
-	print("new_y_hat", new_y_hat)
+	#print("new_predict", new_predict)
+	#print("new_y_hat", new_y_hat)
 	gd.plot(x_scaled, x, y, thetas, new_y_hat)
-
 
 if __name__ == "__main__":
 	main()
